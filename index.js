3d7594b17d0602b5689f828247887e86347e9dc0
Legeron Quentin <quentin.legeron@edu.devinci.fr>
2023-07-07 16:53:54 +0200

    :tada: Ajout des fonctions pour changer le fichier csv et ajout d'un test unitaire

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-// function getRandomNumber() {
-//     return Math.floor(Math.random() * 100);
-// }
+const csv = require('csv-parser');
+const fs = require('fs');
 
-// module.exports = getRandomNumber;
+function updateCSV() {
+  const results = [];
 
-return 
+  fs.createReadStream('students.csv')
+    .pipe(csv())
+    .on('data', (data) => {
+      if (!data.Note) {
+        data.Note = getRandomNumber(0, 20).toString();
+      }
+      results.push(data);
+    })
+    .on('end', () => {
+      const ws = fs.createWriteStream('students.csv');
+      const headers = Object.keys(results[0]).join(',');
+      ws.write(headers + '\n');
+      const averageRowIndex = results.findIndex((row) => row.Prenom === 'Average');
+      if (averageRowIndex !== -1) {
+        results.splice(averageRowIndex, 1);
+      }
+
+
+      results.forEach((data) => {
+        const row = Object.values(data).join(',');
+        ws.write(row + '\n');
+      });
+
+
+      const sum = results.reduce((acc, row) => acc + parseInt(row.Note || 0), 0);
+      const average = sum / results.length;
+
+      const averageRow = { Student_pk: '', Nom: '', Prenom: 'Average', Note: average };
+      const averageRowString = Object.values(averageRow).join(',');
+      ws.write(averageRowString + '\n');
+      
+
+      ws.end();
+    });
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+updateCSV();
+
+module.exports = { updateCSV };
--- index.test.js
+++ index.test.js
@@ -1,8 +1,32 @@
-// const getRandomNumber = require('./index');
+const fs = require('fs');
+const { updateCSV } = require('./index');
 
-// test('it should return a number', () => {
-//     const number = getRandomNumber();
-//     expect(number).toBeGreaterThanOrEqual(0);
-//     expect(number).toBeLessThanOrEqual(100);
-// });
+test('CSV file is updated correctly', () => {
+  // backup des fichiers csv
+  fs.copyFileSync('students.csv', 'backup.csv');
 
+  updateCSV();
+
+  // Check CSV s'il a bien été update
+
+  const updatedContent = fs.readFileSync('students.csv', 'utf8');
+  const rows = updatedContent.trim().split('\n');
+
+  // Check Si la colonne "Note" a bien été ajoutée à chaque ligne
+  for (let i = 1; i < rows.length; i++) {
+    const rowData = rows[i].split(',');
+    const noteValue = rowData[3];
+    expect(noteValue).toBeTruthy(); // check si la valeur de "Note" existe
+    expect(parseInt(noteValue)).toBeGreaterThanOrEqual(0); // Check si la valeur de la note est bien entre 0 et 20
+    expect(parseInt(noteValue)).toBeLessThanOrEqual(20);
+  }
+
+  // Check si la ligne "Average" a bien été ajoutée à la fin du fichier
+  const lastRow = rows[rows.length - 1].split(',');
+  expect(lastRow[2]).toBe('Average');
+  expect(parseFloat(lastRow[3])).toBeDefined(); // Check s'il y a bien une valeur pour la note de la ligne "Average"
+
+  // Remettre les fichiers csv d'origine grace au backup
+  fs.copyFileSync('backup.csv', 'students.csv');
+  fs.unlinkSync('backup.csv');
+});
